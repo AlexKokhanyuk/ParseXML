1b18e18cb9087214782a6f6d5e3dbaa47acba9d1
ayeayedp <aye.aye.dp@gmail.com>
2022-01-14 20:05:20 +0200

    add resurses

--- src/main/java/DoParseXml.java
+++ src/main/java/DoParseXml.java
@@ -1,8 +1,59 @@
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+import java.io.FileNotFoundException;
+
 public class DoParseXml {
+
     public LinkedList getListOfGoods(String file) {
         LinkedList list = new LinkedList();
-
-
+        String kodGoods;
+        char kod[] = new char[3];
+        char id[] = new char[11];
+        String fileName = new String(file);
+        try {
+            File inputFile = new File(fileName);
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(inputFile);
+            doc.getDocumentElement().normalize();
+            NodeList nList = doc.getElementsByTagName("Товар");
+            for (int i = 0; i < nList.getLength(); i++) {
+                Goods goods = new Goods();
+                Node nNode = nList.item(i);
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element eElement = (Element) nNode;
+                    goods.setName(eElement.getElementsByTagName("Наименование")
+                            .item(0).getTextContent());
+                    goods.setId(eElement.getElementsByTagName("Ид")
+                            .item(0).getTextContent());
+                    for (int j = 1; j < 8; j++) {
+                        kodGoods = eElement
+                                .getElementsByTagName("ЗначениеРеквизита")
+                                .item(j).getTextContent().strip();
+                        kodGoods.getChars(0, 3, kod, 0);
+                        if (kod[0] == 'К' & kod[1] == 'о' & kod[2] == 'д') {
+                            int leng = kodGoods.length();
+                            kodGoods.getChars(leng - 11, leng, id, 0);
+                            goods.setKod(Integer.parseInt(new String(id)));
+                            break;
+                        }
+                    }
+                }
+                list.insert(goods);
+            }
+        } catch (NullPointerException e) {
+            System.out.println("Выход за пределы значений реквизита");
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return list;
     }
 }
--- src/main/java/ParseXml.java
+++ src/main/java/ParseXml.java
@@ -14,82 +14,24 @@ import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 import org.w3c.dom.Node;
 import org.w3c.dom.Element;
-
-
 public class ParseXml {
-
-
     public static void main(String[] args) {
 
-//        String nameGoods;
-        String kodGoods;
-
-        char kod[] = new char[3];
-        char id[] = new char[11];
-        LinkedList list = new LinkedList();
-        String fileName = new String(".\\src\\main\\resources\\import.xml");
-
-        try {
-            File inputFile = new File(fileName);
-            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-            Document doc = dBuilder.parse(inputFile);
-            doc.getDocumentElement().normalize();
-//            System.out.println("Root element: " + doc.getDocumentElement().getNodeName());
-            NodeList nList = doc.getElementsByTagName("Товар");
-
-            for (int i = 0; i < nList.getLength(); i++) {
-                Goods goods = new Goods();
-                Node nNode = nList.item(i);
-                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-                    Element eElement = (Element) nNode;
-                    goods.setName(eElement.getElementsByTagName("Наименование")
-                            .item(0).getTextContent());
-                    goods.setId(eElement.getElementsByTagName("Ид")
-                            .item(0).getTextContent());
-                    for (int j = 1; j < 8; j++) {
-                        kodGoods = eElement
-                                .getElementsByTagName("ЗначениеРеквизита")
-                                .item(j).getTextContent().strip();
-                        kodGoods.getChars(0, 3, kod, 0);
-                        if (kod[0] == 'К' & kod[1] == 'о' & kod[2] == 'д') {
-                            int leng = kodGoods.length();
-                            kodGoods.getChars(leng - 11, leng, id, 0);
-                            goods.setKod(Integer.parseInt(new String(id)));
-                            break;
-                        }
-                    }
-                }
-                list.insert(goods);
-            }
-        } catch (NullPointerException e) {
-            System.out.println("Выход за пределы значений реквизита");
-        } catch (FileNotFoundException e) {
-            System.out.println("File not found");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
+        DoParseXml doParseXml=new DoParseXml();
+        LinkedList list=doParseXml.getListOfGoods(".\\src\\main\\resources\\import.xml");
         HashMap<String, Goods> mapOfGoods = new HashMap();
-
-//        list.printList();
         int count = 0;
         int countOfZerro = 0;
-
-
         while (list.iteratorHasNext()) {
-
             Goods tempGods = list.iteratorNext();
             mapOfGoods.put(tempGods.getId(), tempGods);
-            int k = tempGods.getKod();
-
-            System.out.println(k);
-            if (k == 0) {
-                 System.out.println(tempGods.getName());
-                countOfZerro++;
-            }
+//            int k = tempGods.getKod();
+//            System.out.println(k);
+//            if (k == 0) {
+//                 System.out.println(tempGods.getName());
+//                countOfZerro++;
+//            }
             count++;
-
         }
         System.out.println("Capacity of LincedList: " + list.getCapacity());
         System.out.println(count + "  " + countOfZerro);
