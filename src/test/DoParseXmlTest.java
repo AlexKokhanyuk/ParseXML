4a1582fdf2e2b709af2f03fce2b89c47aff01109
aye-aye <akoh@i.ua>
2022-01-27 14:19:58 +0200

    fix DoParseXML

--- src/test/DoParseXmlTest.java
+++ src/test/DoParseXmlTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,14 +8,14 @@ import static org.junit.jupiter.api.Assertions.*;
 class DoParseXmlTest {
 
     @Test
-    void getGods(){
-        DoParseXml doParseXml=new DoParseXml();
+    void getGods() {
+        DoParseXml doParseXml = new DoParseXml();
         String fileOfGoods01 = ".\\src\\main\\resources\\import___5493abdf-394d-49b9-bd4c-736cbaed61d0.xml";
         String fileOfPrises = ".\\src\\main\\resources\\prices___aa9533e0-4970-4592-9f9f-6480fdf8b99c.xml";
         String fileOfStock = ".\\src\\main\\resources\\rests___5a4c78e7-fca4-4a4f-bc9c-6f849b4f3253.xml";
 
 
-        HashMap<String,Goods> mapOfGoods=doParseXml.getGods(fileOfGoods01,fileOfPrises,fileOfStock);
+        HashMap<String, Goods> mapOfGoods = doParseXml.getGods(fileOfGoods01, fileOfPrises, fileOfStock);
         mapOfGoods.forEach((sId, goods) -> System.out.println(goods.getName()
                 + ", prise: " + goods.getPriseCommon() + " $ Stock: " + goods.getStockBalanse()));
         System.out.println("Size of map: " + mapOfGoods.size());
@@ -50,6 +51,33 @@ class DoParseXmlTest {
 
     @Test
     void getListOfGoods() {
+        DoParseXml doParseXml = new DoParseXml();
+        String fileOfGoods01 = ".\\src\\main\\resources\\import___5493abdf-394d-49b9-bd4c-736cbaed61d0.xml";
+        String fileOfPrises = ".\\src\\main\\resources\\prices___aa9533e0-4970-4592-9f9f-6480fdf8b99c.xml";
+        String fileOfStock = ".\\src\\main\\resources\\rests___5a4c78e7-fca4-4a4f-bc9c-6f849b4f3253.xml";
+
+        FileReader fileReader = new FileReader();
+        ArrayList<FilePathAndTime> listOfFiles = fileReader.readFileNameFromFolder(".\\src\\main\\resources\\");
+        FilePathAndTime[] arrayOfFiles = listOfFiles.toArray(new FilePathAndTime[0]);
+
+        String fImport = arrayOfFiles[1].getFilePath();
+//        String fImport = String.valueOf(arrayOfFiles[1].getFilePath());
+
+
+        LinkedList list = doParseXml.getListOfGoods(fImport);
+//        LinkedList list = doParseXml.getListOfGoods(fileOfGoods01);
+        HashMap<String, Goods> mapOfGoods = new HashMap();
+        int count = 0;
+        int countOfZerro = 0;
+        while (list.iteratorHasNext()) {
+            Goods tempGods = (Goods) list.iteratorNext();
+            mapOfGoods.put(tempGods.getId(), tempGods);
+            count++;
+        }
+        mapOfGoods = doParseXml.addPrisesToGoods(mapOfGoods, fileOfPrises);
+        mapOfGoods.forEach((sId, goods) -> System.out.println(goods.getName()
+                + ", prise: " + goods.getPriseCommon() + " $ Stock: " + goods.getStockBalanse()));
+        System.out.println("Size of map: " + mapOfGoods.size());
     }
 
     @Test
--- src/test/FileReaderTest.java
+++ src/test/FileReaderTest.java
@@ -17,9 +17,17 @@ class FileReaderTest {
         FileReader fileReader = new FileReader();
         ArrayList<FilePathAndTime> listOfFiles = fileReader.readFileNameFromFolder(".\\src\\main\\resources\\");
         FilePathAndTime[] arrayOfFiles = listOfFiles.toArray(new FilePathAndTime[0]);
-        for (int i = 0; i < arrayOfFiles.length; i++) {
-            System.out.println(arrayOfFiles[i]);
+        for (int i = 0; i < 4; i++) {
+            System.out.println(arrayOfFiles[i].getFilePath());
         }
+        String fileOfGoods01 = ".\\src\\main\\resources\\import___5493abdf-394d-49b9-bd4c-736cbaed61d0.xml";
+        String fileOfPrises = ".\\src\\main\\resources\\prices___aa9533e0-4970-4592-9f9f-6480fdf8b99c.xml";
+        String fileOfStock = ".\\src\\main\\resources\\rests___5a4c78e7-fca4-4a4f-bc9c-6f849b4f3253.xml";
+        System.out.println(fileOfGoods01);
+        System.out.println(fileOfPrises);
+        System.out.println(fileOfStock);
+
+
         assertEquals(12, arrayOfFiles.length);
     }
 }
